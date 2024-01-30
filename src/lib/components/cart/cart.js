6225149c8c31d9c88f6197112a49874d21a0a929
taherul77 <taherul2317@gmail.com>
2024-01-30 21:50:25 +0600

    header add

--- src/lib/components/cart/cart.js
+++
@@ -1,7 +0,0 @@
-import CartModal from "../modal";
-
-
-export default async function Cart() {
-
-  return <CartModal  />;
-}
---
+++ src/lib/modules/templates/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            hi
+        </div>
+    );
+};
+
+export default Header;
