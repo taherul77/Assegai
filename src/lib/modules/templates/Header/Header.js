701122e660f2539c0bb1b53c2d24e166a1d2222c
taherul77 <taherul2317@gmail.com>
2024-01-31 01:10:55 +0600

    update header and nav

--- src/lib/modules/templates/Header/Header.js
+++ src/lib/modules/templates/Header/Header.js
@@ -12,7 +12,7 @@ import MobileMenuButton from "../components/MobileMenuButton";
 const Header = () => {
   return (
     <div className="bg-gray-100">
-      <div className="mx-auto  px-2 sm:px-4 lg:divide-y lg:divide-gray-500 lg:px-8">
+      <div className="mx-auto  px-2 sm:px-4 lg:divide-y lg:divide-gray-500 lg:px-8 ">
         <div className="relative flex h-14 justify-between">
           <div className="relative z-10 flex px-2 lg:px-0">
             <div className="flex flex-shrink-0 items-center">ASSEGAI</div>
