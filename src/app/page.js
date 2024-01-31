d1c62d0c0100cdb0b18ae672fd0d37e03794d65b
taherul77 <taherul2317@gmail.com>
2024-01-31 20:45:02 +0600

    add home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,9 @@
-import Image from "next/image";
+import HomePage from "@/lib/components/Home/HomePage";
 
 export default function Home() {
   return (
-   <>
-   hi
-   
-   </>
+    <>
+      <HomePage></HomePage>
+    </>
   );
 }
